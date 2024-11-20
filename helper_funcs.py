869ff64ef10154b8a068f7db5172e55efe308569
siri27d <chelluboinas@iitbhilai.ac.in>
2024-11-20 07:30:01 +0530

    update2

--- helper_funcs.py
+++ helper_funcs.py
@@ -2,7 +2,9 @@ import random
 import numpy as np
 import pickle
 import json
-
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
 def load_pickle_data(filename):
     with open(filename, 'rb') as f:
         data = pickle.load(f)
@@ -49,3 +51,28 @@ def init_pos_embs(embedding_dim_pos, max_position=200):
     embedding_pos = [np.zeros(embedding_dim_pos)]
     embedding_pos.extend(np.random.normal(0.0, 0.1, (max_position, embedding_dim_pos)))
     return np.array(embedding_pos)
+
+def load_w2v(embedding_dim, embedding_dim_pos, train_file_path, embedding_path):
+    # Loading text embs
+    #print('\nLoading embeddings')
+    
+    words = load_all_words(train_file_path)
+    word_idx = {word: idx + 1 for idx, word in enumerate(words)}
+    word_idx_rev = {idx + 1: word for idx, word in enumerate(words)}
+
+    w2v = load_glove(embedding_path)
+    embedding = init_embs(words, w2v, embedding_dim)
+
+    embedding_pos = init_pos_embs(embedding_dim_pos)
+    #print('Embeddings loaded.')
+
+    return embedding, embedding_pos, word_idx, word_idx_rev
+
+def normalize_embeddings(embeddings):
+    # Normalize embeddings
+    data = embeddings[1:, :]  # Exclude the first row
+    min_vals = torch.min(data, dim=0, keepdim=True).values
+    max_vals = torch.max(data, dim=0, keepdim=True).values
+    data = (data - min_vals) / (max_vals - min_vals + 1e-8)
+    embeddings[1:, :] = data
+    return embeddings
